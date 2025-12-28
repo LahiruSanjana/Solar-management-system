d5c04ccd0733c1f61eb83b2ec447adf248155957
LahiruSanjana <sanjanaf41@gmail.com>
2025-12-28 18:19:49 +0530

    refactor: Remove unused import of Maindashboard from Usersidebar component

--- Front-end/src/page/admin/components/user/Usersidebar.jsx
+++ Front-end/src/page/admin/components/user/Usersidebar.jsx
@@ -1,7 +1,6 @@
 import { AppSidebar } from "@/Component/Appslide";
 import { LayoutDashboard ,Settings,CircleUserRound,Receipt} from "lucide-react";
 import { SidebarProvider,SidebarTrigger } from "@/components/ui/sidebar";
-import Maindashboard from "./Maindashboard";
 import Userdetails from "./Userdetails";
 
 const Usersidebar = () => {
